2c3cb2cdc2359513a1d4ba5794d54eb207763c70
med yassin jrad <jradmedyassin@gmail.com>
2024-12-04 18:00:58 +0100

    first commit

---
+++ Includes/dbcon.php
@@ -0,0 +1,14 @@
+<?php
+	$host = "localhost";
+	$user = "root";
+	$pass = "";
+	$db = "attendancemsystem";
+	
+	$conn = new mysqli($host, $user, $pass, $db);
+	// Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } else {
+        echo "Database connected successfully!";
+    }
+?>
